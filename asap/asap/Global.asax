4fd2e970af349143738f62020c5647d6ed3cd2a5
midievil <artemiogro@gmail.com>
2017-08-16 21:20:51 +0300

    Initial Commit

---
+++ asap/asap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asap.MvcApplication" Language="C#" %>
---
+++ asap/asap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asap.Startup))]
+namespace asap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
